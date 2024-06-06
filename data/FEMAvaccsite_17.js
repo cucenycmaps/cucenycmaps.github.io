f63e048d0e925d75f7aeeaed6169831e6781865e
lelarobinsonmapping <69472714+lelarobinsonmapping@users.noreply.github.com>
2024-06-06 00:38:00 -0400

    update
    24/6/6 update

---
+++ data/FEMAvaccsite_17.js
@@ -0,0 +1,11 @@
+var json_FEMAvaccsite_17 = {
+"type": "FeatureCollection",
+"name": "FEMAvaccsite_17",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "site name": "New Jerusalem Worship Center", "address": "12205 Smith St, Jamaica, NY 11434", "latitude": 40.68, "longitude": -73.778 }, "geometry": { "type": "Point", "coordinates": [ -73.778, 40.68 ] } },
+{ "type": "Feature", "properties": { "site name": "Church of God of East NY", "address": "913 Suttler Ave, Brooklyn NY 11207", "latitude": 40.671, "longitude": -73.885 }, "geometry": { "type": "Point", "coordinates": [ -73.885, 40.671 ] } },
+{ "type": "Feature", "properties": { "site name": "Concent Avenue Baptist Church", "address": "420 W 45th St, Manhattan NY 10031", "latitude": 40.824, "longitude": -73.946 }, "geometry": { "type": "Point", "coordinates": [ -73.946, 40.824 ] } },
+{ "type": "Feature", "properties": { "site name": "Fort Washington Collegiate Church", "address": "729 W 181st St, Manhattan NY 10033", "latitude": 40.851, "longitude": -73.937 }, "geometry": { "type": "Point", "coordinates": [ -73.937, 40.851 ] } }
+]
+}
